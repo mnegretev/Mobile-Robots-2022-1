bf1e3ae80a86e9aef1ceaae44d0e0bdea745c4a8
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-09-07 17:28:53 -0500

    nombre

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Gutierrez_Alcibar"
 
 def callback_scan(msg):
     global obstacle_detected
