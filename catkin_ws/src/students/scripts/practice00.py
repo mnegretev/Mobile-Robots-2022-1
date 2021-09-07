38a79dd885fa4141faabac19e0e78af77250cc36
Miguel Onate <garcia.miguel.onate@gmail.com>
2021-09-07 17:26:06 -0500

    mis cambios

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -21,11 +21,11 @@ def callback_scan(msg):
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
-    #
+    obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
     return
 
 def main():
-    print "PRACTICE 00 - " + NAME
+    print("PRACTICE 00 - " + NAME)
     rospy.init_node("practice00")
     rospy.Subscriber("/scan", LaserScan, callback_scan)
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -40,7 +40,9 @@ def main():
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        #
+        msg = Twist()
+        msg.linear.x = 0.3 if not obstacle_detected else 0.0
+        pub_cmd_vel.publish(msg)
         loop.sleep()
 
 
