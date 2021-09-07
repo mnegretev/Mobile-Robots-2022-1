5e5c47872dcd26c4f7a0db8e8db4da9859da75f4
Jesús Eduardo Flores González <marvin@hitchhikersguidetothegalaxy.com>
2021-09-07 17:37:28 -0500

    Practica Terminada

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Flores Gonzalez"
 
 def callback_scan(msg):
     global obstacle_detected
@@ -22,14 +22,16 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
+    obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
+    #print(obstacle_detected)
     return
 
 def main():
-    print "PRACTICE 00 - " + NAME
-    rospy.init_node("practice00")
-    rospy.Subscriber("/scan", LaserScan, callback_scan)
-    pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-    loop = rospy.Rate(10)
+    print("PRACTICE 00 - " + NAME)
+    rospy.init_node("practice00") #Es para iniciar el nodo principal de ROS
+    rospy.Subscriber("/scan", LaserScan, callback_scan) #Es para crear el nodo subscritor, en ella se pasa el nombre del topico al que me quiero suscribir, el tipo, callback es uan funcion que se ejecuta por cada lectura que va llegar generando una notificacion.
+    pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10) #Twist velocidad en el espacio
+    loop = rospy.Rate(10) #Variable en Hz
     
     global obstacle_detected
     obstacle_detected = False
@@ -41,7 +43,10 @@ def main():
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
         #
-        loop.sleep()
+	msg = Twist()
+	msg.linear.x = 0.3 if not obstacle_detected else 0.0
+	pub_cmd_vel.publish(msg)
+        loop.sleep() #Es un retrazo para el procesador pero especificamente al de 10Hz aproximadamente tardaria 10ms
 
 
 if __name__ == '__main__':
