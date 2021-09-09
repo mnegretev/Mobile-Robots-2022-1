c7640e650ad587bd3f4dc7136d32923ec98efcdd
Miguel Onate <garcia.miguel.onate@gmail.com>
2021-09-09 17:10:38 -0500

    hola

--- catkin_ws/src/students/scripts/practice00.py
+++ catkin_ws/src/students/scripts/practice00.py
@@ -16,12 +16,12 @@ from geometry_msgs.msg import Twist
 NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
 
 def callback_scan(msg):
-    global obstacle_detected
+    global obstaculo
     #
     # TODO:
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
-    obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
+    obstaculo = msg.ranges[len(msg.ranges)//2] < 1.0
     return
 
 def main():
@@ -31,18 +31,13 @@ def main():
     pub_cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     loop = rospy.Rate(10)
     
-    global obstacle_detected
-    obstacle_detected = False
+    global obstaculo
+    obstaculo = False
+    velocidad = Twist()
     while not rospy.is_shutdown():
-        #
-        # TODO:
-        # Declare a Twist message and assign the appropiate speeds:
-        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
-        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
-        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        msg = Twist()
-        msg.linear.x = 0.3 if not obstacle_detected else 0.0
-        pub_cmd_vel.publish(msg)
+        
+        velocidad.linear.x = 0 if obstaculo else 0.5
+        pub_cmd_vel.publish(velocidad)
         loop.sleep()
 
 
