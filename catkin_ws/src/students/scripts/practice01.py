46dd72d0510a9074f507fa68b613fb893ea0c2d0
Machc11 <marco.chavezc@outlook.com>
2021-09-23 10:43:31 -0700

    Practica 1 completada

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -7,7 +7,7 @@
 # Write the code necesary to get an inflated map and a cost map given
 # an inflation radius and a cost radius.
 #
-
+import sys
 import rospy
 import numpy
 from nav_msgs.msg import OccupancyGrid
@@ -15,7 +15,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
+NAME = "Chavez_Contreras_Marco_Antonio"
 
 def get_inflated_map(static_map, inflation_cells):
     print("Inflating map by " + str(inflation_cells) + " cells")
@@ -28,6 +28,13 @@ def get_inflated_map(static_map, inflation_cells):
     # Map is given in 'static_map' as a bidimensional numpy array.
     # Consider as occupied cells all cells with an occupation value greater than 50
     #
+    for i in range(height): 
+         for j in range(width): 
+             if static_map[i,j]==100: 
+                 for k1 in range(i-inflation_cells, i+inflation_cells+1):  
+                     for k2 in range(j-inflation_cells, j+inflation_cells+1): 
+			inflated[k1,k2] = static_map[i,j]
+
     return inflated
 
 def get_cost_map(static_map, cost_radius):
@@ -55,7 +62,14 @@ def get_cost_map(static_map, cost_radius):
     #  [ 3 X 3 3 3 2]
     #  [ 3 3 3 X 3 2]]
     # Cost_radius indicate the number of cells around obstacles with costs greater than zero.
-    
+    for i in range(height):
+    	for j in range(width):
+   		if static_map[i,j]==100:
+			for k1 in range(-cost_radius, cost_radius + 1):
+				for k2 in range(-cost_radius, cost_radius + 1):
+					costo= cost_radius-max(abs(k1),abs(k2))
+					mapacosto= cost_map[i+k1, j+k2]
+					cost_map[i + k1, j + k2]=max(costo,mapacosto)
     return cost_map
 
 def callback_inflated_map(req):
