b440615ae0053f8de4520f756a4d508d88681bc9
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-09-23 02:46:46 -0500

    modified script practice01 inflation costos

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 #
 # AUTONOMOUS MOBILE ROBOTS - UNAM, FI, 2022-1
-# PRACTICE 1 - INFLATION AND COST MAPS
+# PRACTICE 01 - INFLATION AND COST MAPS
 #
 # Instructions:
 # Write the code necesary to get an inflated map and a cost map given
 # an inflation radius and a cost radius.
 #
 
+import sys
 import rospy
 import numpy
 from nav_msgs.msg import OccupancyGrid
@@ -15,8 +16,8 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
-
+NAME = "CRUZ_TORRES_SOFIA_ANGELICA"
+static_map = None
 def get_inflated_map(static_map, inflation_cells):
     print("Inflating map by " + str(inflation_cells) + " cells")
     inflated = numpy.copy(static_map)
@@ -28,6 +29,12 @@ def get_inflated_map(static_map, inflation_cells):
     # Map is given in 'static_map' as a bidimensional numpy array.
     # Consider as occupied cells all cells with an occupation value greater than 50
     #
+    for i in range(height ): #Va a rrecorrer el # de renglones
+         for j in range(width): #Va a  recorrer todas las columnas de esos renglones
+             if static_map[i,j]>0: 
+                 for k1 in range(i-inflation_cells, i+inflation_cells+1):  
+                     for k2 in range(j-inflation_cells, j+inflation_cells+1): 
+			inflated[k1,k2] = static_map[i,j]
     return inflated
 
 def get_cost_map(static_map, cost_radius):
@@ -56,6 +63,15 @@ def get_cost_map(static_map, cost_radius):
     #  [ 3 3 3 X 3 2]]
     # Cost_radius indicate the number of cells around obstacles with costs greater than zero.
     
+    for i in range(height ): #Va a rrecorrer el # de renglones
+         for j in range(width): #Va a  recorrer todas las columnas de esos renglones
+             if static_map[i,j]>0: 
+                 for k1 in range(-cost_radius,cost_radius+1):   
+                     for k2 in range(-cost_radius, cost_radius+1): 
+                         c_rad= cost_radius- max(abs(k1),abs(k2)) +1  
+                         c_map= cost_map[i+k1, j+k2]
+                         while c_map>c_rad:
+                            cost_map[i+k1, j+k2] = max(c_rad, c_map)
     return cost_map
 
 def callback_inflated_map(req):
@@ -105,4 +121,3 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-    
