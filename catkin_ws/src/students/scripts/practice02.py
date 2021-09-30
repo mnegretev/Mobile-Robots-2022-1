0481436747208970e1a2e7fad040472f86b97547
Jesús Eduardo Flores González <57874646+JEFMX@users.noreply.github.com>
2021-09-29 19:43:07 -0500

    Practica 2 Terminada olvide poner mi nombre

--- catkin_ws/src/students/scripts/practice02.py
+++ catkin_ws/src/students/scripts/practice02.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Flores González"
 
 msg_path = Path()
 
