01c13a1e73655f027f58b24778a7d033a3ff2dff
Alfonso Murrieta <alfonsomvmx@outlook.com>
2021-09-30 18:59:53 -0700

    Practice 3: Completed*
    Error using scipy to get normal of nabla, I really recommend use numpy better :/

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -9,6 +9,7 @@
 #
 
 import numpy
+#import scipy.linalg
 import heapq
 import rospy
 from nav_msgs.msg import Path
@@ -16,7 +17,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from custom_msgs.srv import SmoothPath
 from custom_msgs.srv import SmoothPathResponse
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Alfonso Murrieta Villegas"
 
 msg_smooth_path = Path()
 
@@ -24,18 +25,53 @@ def smooth_path(Q, alpha, beta):
     print("Smoothing path with params: " + str([alpha,beta]))
     #
     # TODO:
-    # Write the code to smooth the path Q, using the gradient descend algorithm,
+    # Write the code to smooth the path Q, using the nabla descend algorithm,
     # and return a new smoothed path P.
     # Path is composed of a set of points [x,y] as follows:
     # [[x0,y0], [x1,y1], ..., [xn,ym]].
     # The smoothed path must have the same shape.
     # Return the smoothed path.
     #
+
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
+    epsilon = 0.1
     
+    #PRACTICE 3
+    # Auxiliar values to go inside of while
+    nabla_mag = 1 
+    steps = 1 
+
+    while nabla_mag > tol and steps < 10000:
+        
+        #Change nabla value to modify after with the normal
+        nabla_mag = 0
+        sizeP = len(P) - 1
+
+        #Auxiliar for to fill values of position 0 and 1
+        for i in range(0,1):
+            nabla[0][i] = alpha* (P[0][i] - Q[0][i]) - beta * (P[1][i]-P[0][i])
+            P[0][i] = P[0][i] - epsilon * nabla[0][i]
+
+        #Algorithm
+        for i in range(1,sizeP - 1):
+            for j in range(0,1):
+                nabla[i][j] = alpha * (P[i][j] - Q[i][j]) + beta * (2*P[i][j] - P[i-1][j] - P[i+1][j])
+                P[i][j] = P[i][j] - epsilon * nabla[i][j]
+            
+        for i in range(0,1):
+            nabla[sizeP][i] = alpha * (P[sizeP][i] - Q[sizeP][i]) + beta * (P[sizeP][i] - P[sizeP-1][i])
+            P[sizeP][i] = P[sizeP][i] - epsilon * nabla[sizeP][i]
+
+        #Modify variables to decide if stay on while
+
+        # TO NOT install scipy I decided to use numpy
+        #nabla_mag = scipy.linalg.norm(nabla)
+        nabla_mag = numpy.linalg.norm(nabla)
+        steps += 1 
+        
+    print("PROCESS HAS DONE")               
     return P
 
 def callback_smooth_path(req):
@@ -63,4 +99,5 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-    
+
+
