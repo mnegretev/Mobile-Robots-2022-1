8081dd03ce976d93da8a5822911af3aaceb3d874
Jorge Luis Manzo Soto <luismanzosoto@hotmail.com>
2021-10-06 17:45:03 -0500

    Practica Terminada

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from custom_msgs.srv import SmoothPath
 from custom_msgs.srv import SmoothPathResponse
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Manzo_Soto_Jorge_Luis"
 
 msg_smooth_path = Path()
 
@@ -34,8 +34,29 @@ def smooth_path(Q, alpha, beta):
     P = numpy.copy(Q)
     tol     = 0.00001                   
     nabla   = numpy.full(Q.shape, float("inf"))
-    epsilon = 0.1                       
-    
+    epsilon = 0.1  
+    # Despues de considerar que nuestro primer y ultimo termino van a ser cero se progaman los siguientes comandos 
+    # Primero indicamos que nuestro primer termino de nabla sera cero de la siguiente manera.                
+    nabla[0]=0
+    # Posteriormente indicamos que nuestro ultimo termino tambien sera cero, como se muestra a continuacion.
+    nabla[len(nabla)-1]=0
+    # Inicializamos nuestra variable steps en cero para comenzar con la operacion.
+    steps=0
+    # En la siguiente condicional que si la magnitud de nuestra variable nabla sea mayor que tol, que se trata de un 
+    # valor cercano en cero y que nuestra variable steps sea menor que 1000, se comienza con la operacion.
+    while numpy.linalg.norm(nabla) > tol and steps <100000:
+    # Con este for calculamos el gradiente que necesitamos para marcar la ruta, aqui indicamos que nuestra variable i
+    # comenzara desde el punto 1, hasta el ultimo punto de nuestra nabla, para esto calculamos nabla.
+      for i in range (1, len(nabla)-1): 
+    # Para el calculo de nabla aplicamos lo calculado en clase, lo cual es lo siguiente.
+    # nabla=alpha por el iesimo punto menos el correspondiente punto de la ruta original mas beta por dos veces el 
+    # iesimo punto, menos el punto posterior, menos el punto anterior. 
+        nabla[i]=beta*(P[i]-Q[i])+alpha*(2*P[i]-P[i-1]-P[i+1])
+    # Posteriormente, indicamos que el conjunto de nuestros puntos se van a mover en sentido contrario a la epsilon por 
+    # nuestro gradiente que lleva por nombre nabla
+      P=P-epsilon*nabla
+    # Por ultimo, le sumamos una unidad a nuestra variable step 
+      steps+=1
     return P
 
 def callback_smooth_path(req):
