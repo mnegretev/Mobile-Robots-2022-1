ce5604bc9c476afa3208877058ce7978f9548f58
JuarezCastilloA <jucart3.1@gmail.com>
2021-10-06 15:37:54 -0500

    Entrega de practica 3

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -16,7 +16,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from custom_msgs.srv import SmoothPath
 from custom_msgs.srv import SmoothPathResponse
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Juarez_Castillo"
 
 msg_smooth_path = Path()
 
@@ -26,16 +26,32 @@ def smooth_path(Q, alpha, beta):
     # TODO:
     # Write the code to smooth the path Q, using the gradient descend algorithm,
     # and return a new smoothed path P.
-    # Path is composed of a set of points [x,y] as follows:
+    # Path Q is composed of a set of points [x,y] as follows:
     # [[x0,y0], [x1,y1], ..., [xn,ym]].
     # The smoothed path must have the same shape.
     # Return the smoothed path.
     #
     P = numpy.copy(Q)
-    tol     = 0.00001                   
+    tol     = 0.0001                   
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1                       
-    
+    #
+    #q[i]: puntos ruta original
+    #p[i]: puntos ruta suave
+    #Nabla: Funcion de costo
+
+    #Para no mover los puntos inicial y final:
+    nabla[0]=0
+    nabla[-1]=0 
+    steps=1
+
+    #Algoritmo descenso del gradiente
+    while numpy.linalg.norm(nabla)>tol and steps < 100000:
+        for i in range(1, (len(Q)-1)):
+            nabla[i]=beta*(P[i]-Q[i]) + alpha*(2*P[i]-P[i-1]-P[i+1])
+        P=P-epsilon*nabla
+        steps +=1
+    print("Listo")
     return P
 
 def callback_smooth_path(req):
@@ -48,7 +64,7 @@ def callback_smooth_path(req):
     return SmoothPathResponse(smooth_path=msg_smooth_path)
 
 def main():
-    print "PRACTICE 03 - " + NAME
+    print ("PRACTICE 03 - " + NAME)
     rospy.init_node("practice03", anonymous=True)
     rospy.Service('/path_planning/smooth_path', SmoothPath, callback_smooth_path)
     pub_path = rospy.Publisher('/path_planning/smooth_path', Path, queue_size=10)
