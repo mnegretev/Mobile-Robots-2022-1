d675bd0b3f679624341c77e76e95c9a404946def
Miguel Oñate <garcia.miguel.onate@gmail.com>
2021-10-13 23:11:11 -0500

    hola

--- catkin_ws/src/students/scripts/practice03.py
+++ catkin_ws/src/students/scripts/practice03.py
@@ -36,29 +36,14 @@ def smooth_path(Q, alpha, beta):
     nabla   = numpy.full(Q.shape, float("inf"))
     epsilon = 0.1                       
 
-    nabla_mag = 1 
-    pasos = 1 
-
-    while nabla_mag > tol and pasos < 10000:
-        nabla_mag = 0
-        tamano_de_P = len(P) - 1
-        for i in range(0,1):
-            nabla[0][i] = alpha* (P[0][i] - Q[0][i]) - beta * (P[1][i]-P[0][i])
-            P[0][i] = P[0][i] - epsilon * nabla[0][i]
-
-        #AQUI COMIENZA LA IMPLEMENTACION DEL ALGORITMO
-        for i in range(1,tamano_de_P - 1):
-            for j in range(0,1):
-                nabla[i][j] = alpha * (P[i][j] - Q[i][j]) + beta * (2*P[i][j] - P[i-1][j] - P[i+1][j])
-                P[i][j] = P[i][j] - epsilon * nabla[i][j]
-            
-        for i in range(0,1):
-            nabla[tamano_de_P][i] = alpha * (P[tamano_de_P][i] - Q[tamano_de_P][i]) + beta * (P[tamano_de_P][i] - P[tamano_de_P-1][i])
-            P[tamano_de_P][i] = P[tamano_de_P][i] - epsilon * nabla[tamano_de_P][i]
-
-        nabla_mag = numpy.linalg.norm(nabla) #ESTE TAMBIEN ME REGRESA LA NORMA DE NABLA, SOLO QUE ES DE NUMPY
-        pasos += 1 
-                      
+    nabla[0] = 0
+    nabla[-1] = 0
+    pasos=0
+    while numpy.linalg.norm(nabla) > tol and pasos < 10000:
+        for i in range(1, len(nabla)-1):
+            nabla[i] = beta*(P[i] - Q[i]) + alpha*(2*P[i] - P[i-1] - P[i+1])
+        P = P - epsilon*nabla
+        pasos+=1
     return P
 
 def callback_smooth_path(req):
