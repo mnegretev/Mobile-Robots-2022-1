9946b9e6004bb702d6f3c52df2f11314bad3d394
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-10-14 13:41:27 -0500

    Practica 4 terminada

--- catkin_ws/src/students/scripts/practice04.py
+++ catkin_ws/src/students/scripts/practice04.py
@@ -17,7 +17,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from custom_msgs.srv import SmoothPath, SmoothPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "VARILLA_MEJIA"
 
 pub_cmd_vel = None
 loop        = None
@@ -40,10 +40,25 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
-    
+    alpha = 1
+    beta = 1
+    v_max = 0.5
+    w_max = 2*math.pi
+    error_a = math.atan2(goal_y - robot_y, goal_x - robot_x) - robot_a}
+
+    if error_a > math.pi:
+        error_a -= 2*math.pi
+    if error_a <= -math.pi
+    error_a += 2*math.pi
+
+    v= v_max*math.exp(-error_a*error_a/alpha)
+    w= w_max*(2/(1 + math.exp(-error_a/beta)) -1)
+    cmd_vel.linear.x = v
+    cmd_vel.angular.z = w
     return cmd_vel
 
 def follow_path(path):
+    
     #
     # TODO:
     # Use the calculate_control function to move the robot along the path.
@@ -67,6 +82,26 @@ def follow_path(path):
     #     Calculate global error
     # Send zero speeds (otherwise, robot will keep moving after reaching last point)
     #
+
+    idx = 0
+    [local_x, local_y] = path[idx]
+    [global_x, global_y] = path[-1]
+    [robot_x, robot_y, robot_a] = get_robot_pose(listener)
+
+    global_error = math.sqrt((global_x - robot_x)**2 + (global_y - robot_y)**2)
+    local_error = math.sqrt((local_x - robot_x)**2 + (local_y - robot_y)**2)
+
+    while global_error > 0.1 and not rospy.is.shutdown():
+        pub_cmd_vel.publish(calculate_control(robot_x, robot_y, robot_a, local_x, local_y))
+    loop.sleep()
+    [robot_x, robot_y, robot_a] = get_robot_pose(listener)
+    local_error = math.sqrt((local_x - robot_x)**2 + (local_y - robot_y)**2)
+    if local_error < 0.3
+    idx = min(idx+1, len(path)-1)
+
+    [local_x, local_y] = path[idx]
+    global_error = math.sqrt((global_x - robot_x)**2 + (global_y - robot_y)**2)
+    pub_cmd_vel.publish(Twist())
     return
     
 def callback_global_goal(msg):
