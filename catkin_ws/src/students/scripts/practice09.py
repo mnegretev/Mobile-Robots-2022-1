34a4b0c2ee7bff798f72ef6eb4f213c5afd2820f
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-11-18 10:06:36 -0600

    Practica 9 finalizada

--- catkin_ws/src/students/scripts/practice09.py
+++ catkin_ws/src/students/scripts/practice09.py
@@ -17,23 +17,39 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Herrejon Ramirez Osvaldo"
 
 def segment_by_color(img_bgr, points):
     #
     # TODO:
     # - Change color space from RGB to HSV.
     #   Check online documentation for cv2.cvtColor function
+    img_hsv=cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
     # - Determine the pixels whose color is in the color range of the ball.
     #   Check online documentation for cv2.inRange
+    img_bin=cv2.inRange(img_hsv, numpy.array([(25, 200, 50)], numpy.srray([35, 255, 255]))
     # - Calculate the centroid of all pixels in the given color range (ball position).
     #   Check online documentation for cv2.findNonZero and cv2.mean
+    indices=cv2.findNonZero(img_bin)
+    [img_x, img_y, a, b]=cv2.mean(indices)
+    print([img_x, img_y])
     # - Calculate the centroid of the segmented region in the cartesian space
     #   using the point cloud 'points'. Use numpy array notation to process the point cloud data.
     #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
     #   the pixel in the center of the image.
     #
-    return [0,0,0,0,0]
+    [x, y, z, counter]=[0,0,0,0]
+    for[[c,r]] in indices:
+        xt=points[r,c][0]
+        yt=points[r,c][1]
+        zt=points[r,c][2]
+        if math.isnan(xt) or math.isnan(yt) or math.isnan(zt):
+            continue
+        [x, y, z, counter]=[x+xt, y+yt, z+zt, counter+1]
+    x=x/counter if counter>0 else 0
+    y=y/counter if counter>0 else 0
+    z=z/counter if counter>0 else 0
+    return [img_x,img_y,x,y,z]
 
 def callback_point_cloud(msg):
     global pub_point
