612a800612b4d35a9cef6c48537a381966258b03
CarlosMontoya <montoycarlosivan@gmail.com>
2021-11-18 13:52:03 -0600

    Practica 09 Completada

--- catkin_ws/src/students/scripts/practice09.py
+++ catkin_ws/src/students/scripts/practice09.py
@@ -17,7 +17,7 @@ from std_msgs.msg import Header
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import PointStamped, Point
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "montoya_martinez"
 
 def segment_by_color(img_bgr, points):
     #
@@ -33,7 +33,29 @@ def segment_by_color(img_bgr, points):
     #   Example: 'points[240,320][1]' gets the 'y' value of the point corresponding to
     #   the pixel in the center of the image.
     #
-    return [0,0,0,0,0]
+    img_hsv=cv2.cvtColor(img_bgr,cv2.COLOR_BGR2HSV)
+    img_bin=cv2.inRange(img_hsv, numpy.array([25,200,50]),numpy.array([35,255,255]))
+    idx= cv2.findNonZero(img_bin)
+    mean_coords= cv2.mean(idx)
+    [x,y,z,count]=[0,0,0,0]
+
+    for [[c,r]] in idx:
+        p_x = points[r,c][0]
+        p_y = points[r,c][1]
+        p_z = points[r,c][2]
+        if math.isnan(p_x) or math.isnan(p_y) or math.isnan(p_z):
+            continue
+        [x,y,z,count]=[x+p_x, y+p_y, z+p_z, count+1]
+
+    if count>0:
+      x = x/count
+      y = y/count
+      z = z/count
+    else:
+      x = 0
+      y = 0
+      z = 0
+    return [mean_coords[0],mean_coords[1],x,y,z]
 
 def callback_point_cloud(msg):
     global pub_point
