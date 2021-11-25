1b08fb46b2a9a449391749095a0a73ecd6f61798
JoaquinValdespino <pataponjq1@gmail.com>
2021-11-24 23:06:23 -0600

    PRACTICA 10 VALDESPINO MENDIETA

--- catkin_ws/src/students/scripts/practice10.py
+++ catkin_ws/src/students/scripts/practice10.py
@@ -62,9 +62,14 @@ def forward_kinematics(q, Ti, Wi):
     #     Check online documentation of these functions:
     #     http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
     #
-    
-    x,y,z = 0,0,0  # Get xyz from resulting H
-    R,P,Y = 0,0,0  # Get RPY from resulting H
+    H=  tft.identity_matrix()
+    for i in range(len(q)):
+      #I*Rt*Tr
+      H=tft.concatenate_matrices(H,Ti[i],tft.rotation_matrix(q[i],Wi[i]))
+   
+    H=tft.concatenate_matrices(H,Ti[7])
+    x,y,z = H[0][3],H[1][3],H[2][3]  # Get xyz from resulting H
+    R,P,Y = tft.euler_from_matrix(H,'rxyz')  # Get RPY from resulting H
     return numpy.asarray([x,y,z,R,P,Y])
 
 def jacobian(q, Ti, Wi):
@@ -94,6 +99,9 @@ def jacobian(q, Ti, Wi):
     J = numpy.asarray([[0.0 for a in q] for i in range(6)])            # J 6x7 full of zeros
     qn = numpy.asarray([q,]*len(q)) + delta_q*numpy.identity(len(q))   # q_next as indicated above
     qp = numpy.asarray([q,]*len(q)) - delta_q*numpy.identity(len(q))   # q_prev as indicated above
+
+    for i in range (0,7):
+      J[:,i]=(forward_kinematics(qn[i,:],Ti,Wi)-forward_kinematics(qp[i,:],Ti,Wi))/(2*delta_q)
     
     return J
 
@@ -126,7 +134,43 @@ def inverse_kinematics_xyzrpy(x, y, z, roll, pitch, yaw, Ti, Wi):
     #    Return calculated q if maximum iterations were not exceeded
     #    Otherwise, return None
     #
+    p=forward_kinematics(q, Ti, Wi)
+
+    error=p-pd
     
+    for i in range(len(error)):
+      if error[i] > math.pi:
+        error[i]-=2*math.pi
+      elif error[i] < -math.pi:
+        error[i]+=2*math.pi
+    
+    while numpy.linalg.norm(error)>tolerance and iterations<max_iterations:
+      J = jacobian(q,Ti,Wi)
+      q = q - numpy.dot(numpy.linalg.pinv(J),error)
+      for i in range(len(q)):
+        if q[i] > math.pi:
+          q[i]-=2*math.pi
+        elif q[i] < -math.pi:
+          q[i]+=2*math.pi
+
+      p= forward_kinematics(q,Ti,Wi)
+      error=p-pd
+      
+      for i in range(len(error)):
+        if error[i] > math.pi:
+          error[i]-=2*math.pi
+        elif error[i] < -math.pi:
+          error[i]+=2*math.pi
+   
+      iterations+=1
+
+    
+    if(iterations<max_iterations):
+      print("Inverse Kinematic Solved->")
+      print("Iterations: "+str(iterations))
+      print("Information:"+str(q))
+      return q
+
     return None
 
 def callback_la_ik_for_pose(req):
