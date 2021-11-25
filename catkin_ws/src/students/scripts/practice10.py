bba2d1b2abf4b0de845fd9d63f77bc34bf35500f
Jesús Eduardo Flores González <marvin@hitchhikersguidetothegalaxy.com>
2021-11-24 21:11:18 -0600

    practica 10 terminada

--- catkin_ws/src/students/scripts/practice10.py
+++ catkin_ws/src/students/scripts/practice10.py
@@ -20,6 +20,7 @@ from geometry_msgs.msg import PointStamped
 from custom_msgs.msg import ArmConfiguration
 from custom_msgs.srv import *
 
+NAME = "Flores Gonzalez"
 def get_model_info():
     global joints, transforms
     robot_model = urdf_parser_py.urdf.URDF.from_parameter_server()
@@ -41,60 +42,27 @@ def get_model_info():
         transforms['right'].append(tft.concatenate_matrices(T,R))
     
 def forward_kinematics(q, Ti, Wi):
-    #
-    # TODO:
-    # Calculate the forward kinematics given the set of seven angles 'q'
-    # You can use the following steps:
-    #     H = I   # Assing to H a 4x4 identity matrix
-    #     for all qi in q:
-    #         H = H * Ti * Ri
-    #     H = H * Ti[7]
-    #     Get xyzRPY from the resulting Homogeneous Transformation 'H'
-    # Where:
-    #     Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    #     Ri are the Homogeneous Transformations with zero-translation and rotation qi around axis Wi.
-    #     Ti[7] is the final Homogeneous Transformation from gripper center to joint 7.
-    # Hints:
-    #     Use the tft.identity_matrix() function to get the 4x4 I
-    #     Use the tft.concatenate_matrices() function for multiplying Homogeneous Transformations
-    #     Use the tft.rotation_matrix() matrices Ri.
-    #     Use the tft.euler_from_matrix() function to get RPY from matrix H
-    #     Check online documentation of these functions:
-    #     http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
-    #
     
-    x,y,z = 0,0,0  # Get xyz from resulting H
-    R,P,Y = 0,0,0  # Get RPY from resulting H
+    H = tft.identity_matrix()
+    for i in range(len(q)):
+        H = tft.concatenate_matrices(H,Ti[i],tft.rotation_matrix(q[i],Wi[i]))
+
+    H=tft.concatenate_matrices(H,Ti[7])
+
+    x,y,z = H[0][3],H[1][3],H[2][3] 
+    R,P,Y = tft.euler_from_matrix(H,'rxyz')  
     return numpy.asarray([x,y,z,R,P,Y])
 
 def jacobian(q, Ti, Wi):
     delta_q = 0.000001
-    #
-    # TODO:
-    # Calculate the Jacobian given a kinematic description Ti and Wi
-    # where:
-    # Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    # Wi are the axis of rotation of i-th joint
-    # Use the numeric approximation:   f'(x) = (f(x+delta) - f(x-delta))/(2*delta)
-    #
-    # You can do the following steps:
-    #     J = matrix of 6x7 full of zeros
-    #     q_next = [q1+delta       q2        q3   ....     q7
-    #                  q1       q2+delta     q3   ....     q7
-    #                              ....
-    #                  q1          q2        q3   ....   q7+delta]
-    #     q_prev = [q1-delta       q2        q3   ....     q7
-    #                  q1       q2-delta     q3   ....     q7
-    #                              ....
-    #                  q1          q2        q3   ....   q7-delta]
-    #     FOR i = 1,..,7:
-    #           i-th column of J = ( FK(i-th row of q_next) - FK(i-th row of q_prev) ) / (2*delta_q)
-    #     RETURN J
-    #     
+    
     J = numpy.asarray([[0.0 for a in q] for i in range(6)])            # J 6x7 full of zeros
     qn = numpy.asarray([q,]*len(q)) + delta_q*numpy.identity(len(q))   # q_next as indicated above
     qp = numpy.asarray([q,]*len(q)) - delta_q*numpy.identity(len(q))   # q_prev as indicated above
-    
+
+
+    for i in range(0,7):
+        J[:,i]=(forward_kinematics(qn[i,:],Ti,Wi)-forward_kinematics(qp[i,:],Ti,Wi))/(2*delta_q)
     return J
 
 def inverse_kinematics_xyzrpy(x, y, z, roll, pitch, yaw, Ti, Wi):
@@ -103,31 +71,39 @@ def inverse_kinematics_xyzrpy(x, y, z, roll, pitch, yaw, Ti, Wi):
     max_iterations = 20
     iterations = 0
     q = numpy.asarray([-0.5, 0.6, 0.3, 2.0, 0.3, 0.2, 0.3])  # Initial guess
-    #
-    # TODO:
-    # Solve the IK problem given a kinematic description (Ti, Wi) and a desired configuration.
-    # where:
-    # Ti are the Homogeneous Transformations from frame i to frame i-1 when joint i is at zero position
-    # Wi are the axis of rotation of i-th joint
-    # Use the Newton-Raphson method for root finding. (Find the roots of equation FK(q) - pd = 0)
-    # You can do the following steps:
-    #
-    #    Set an initial guess for joints 'q'. Suggested: [-0.5, 0.6, 0.3, 2.0, 0.3, 0.2, 0.3]
-    #    Calculate Forward Kinematics 'p' by calling the corresponding function
-    #    Calcualte error = p - pd
-    #    Ensure orientation angles of error are in [-pi,pi]
-    #    WHILE |error| > TOL and iterations < maximum iterations:
-    #        Calculate Jacobian
-    #        Update q estimation with q = q - pseudo_inverse(J)*error
-    #        Ensure all angles q are in [-pi,pi]
-    #        Recalculate forward kinematics p
-    #        Recalculate error and ensure angles are in [-pi,pi]
-    #        Increment iterations
-    #    Return calculated q if maximum iterations were not exceeded
-    #    Otherwise, return None
-    #
     
-    return None
+    p = forward_kinematics(q, Ti, Wi)
+    
+    error=p-pd 
+   
+    while numpy.linalg.norm(error)>tolerance and iterations<max_iterations: 
+
+       
+        for i in range(len(error)):
+            if error[i] > math.pi:
+                error[i]-=2*math.pi
+            elif error[i] < (-math.pi):
+                error[i]+=2*math.pi
+
+        J = jacobian(q,Ti,Wi)
+        q = q - numpy.dot(numpy.linalg.pinv(J),error)
+
+        for i in range(len(q)):
+            if q[i] > math.pi:
+                q[i]-=2*math.pi
+            elif q[i] < -math.pi:
+                q[i]+=2*math.pi
+      
+        p= forward_kinematics(q,Ti,Wi)
+        error=p-pd
+
+        iterations+=1 
+ 
+    if(iterations<max_iterations):
+        return q
+   
+    else:     
+        return None
 
 def callback_la_ik_for_pose(req):
     global transforms, joints
@@ -170,6 +146,7 @@ def callback_ra_fk(req):
     return resp
 
 def main():
+    print "PRACTICE 10 - " + NAME
     print("INITIALIZING INVERSE KINEMATIC NODE BY MARCOSOFT...")
     rospy.init_node("ik_geometric")
     get_model_info()
