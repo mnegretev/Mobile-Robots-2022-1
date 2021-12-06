ffac9b46591d2c9c964b1c67ca7e1b6078e1d607
Alfonso Murrieta <alfonsomvmx@outlook.com>
2021-12-05 18:59:11 -0800

    vel/w  for final project

--- catkin_ws/src/students/scripts/practice04.py
+++ catkin_ws/src/students/scripts/practice04.py
@@ -42,7 +42,7 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
 
     # v = v_max*math.exp(-error_a*error_a/alpha)
     # w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
-    v = 0.5*math.exp(-error_a*error_a/alpha)
+    v = 0.3*math.exp(-error_a*error_a/alpha)
     w = 0.5*(2/(1 + math.exp(-error_a/beta)) - 1)
 
 
