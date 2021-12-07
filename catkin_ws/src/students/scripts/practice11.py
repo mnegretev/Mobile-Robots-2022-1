42cc1a7985ade42bed12944c96d11913a266b4c1
Aldo Hernandez Fontes <marvin@hitchhikersguidetothegalaxy.com>
2021-12-06 18:17:12 -0600

    practica terminada

--- catkin_ws/src/students/scripts/practice11.py
+++ catkin_ws/src/students/scripts/practice11.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "HERNANDEZ FONTES"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -49,6 +49,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
+        y.append(x)
+        for i in range(len(self.layer_sizes)-1):
+            u = numpy.dot(self.weights[i], x) + self.biases[i]
+            x = 1.0/(1.0 + numpy.exp(-u))
+            y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -66,12 +71,19 @@ class NeuralNetwork(object):
         # Calculate delta for the output layer L: delta=(yL-yt)*yL*(1-yL)
         # nabla_b of output layer = delta      
         # nabla_w of output layer = delta*yLpT where yLpT is the transpose of the ouput vector of layer L-1
+        delta = (y[-1]-yt) * y[-1] * (1-y[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta*numpy.transpose(y[-2])
         # FOR all layers 'l' from L-1 to input layer: 
         #     delta = (WT * delta)*yl*(1 - yl)
         #     where 'WT' is the transpose of the matrix of weights of layer l+1 and 'yl' is the output of layer l
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
-        #        
+        #
+        for i in range(2, len(self.layer_sizes)):
+            delta = numpy.dot(numpy.transpose(self.weights[-i+1]), delta) * y[-i] * (1-y[-i])
+            nabla_b[-i] = delta
+            nabla_w[-i] = delta * numpy.transpose(y[-i-1])
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
