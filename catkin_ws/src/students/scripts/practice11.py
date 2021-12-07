6c7cfbb36a7a2db1df27b2f265ef7e21e1557c01
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-12-07 16:15:08 -0600

    Practica 11 terminada

--- catkin_ws/src/students/scripts/practice11.py
+++ catkin_ws/src/students/scripts/practice11.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "VARILLA_MEJIA"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -49,6 +49,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
+        y.append(x)
+	for i in range(2,len(self.biases)):
+            z = numpy.dot(self.weights[i], x) + self.biases[i]
+	    x = 1.0 / (1.0 + numpy.exp(-z))
+            y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -71,7 +76,15 @@ class NeuralNetwork(object):
         #     where 'WT' is the transpose of the matrix of weights of layer l+1 and 'yl' is the output of layer l
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
-        #        
+        #
+        delta = (y[-1]-yt)*y[-1]
+	layer = delta*y[-2].transpose
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta*y[-2].transpose
+	for i in range(2,len(self.biases)):
+		delta=(numpy.dot(self.weights[-i+1].transpose,delta))*y[-i]*(1-y[-i])
+        	nabla_b[-i] = delta
+	        nabla_w[i] = delta*y[-i-1].transpose
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
