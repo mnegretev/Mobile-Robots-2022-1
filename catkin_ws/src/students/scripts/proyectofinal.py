4082f967b5f29099708ea2396f6687b62626519f
GustavoHerrera97 <panke1997@gmail.com>
2021-12-08 03:59:57 -0600

    final.py

---
+++ catkin_ws/src/students/scripts/proyectofinal.py
@@ -0,0 +1,48 @@
+import sys
+import rospy
+from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
+
+robopub = None
+
+def movement(instruction):
+	global moving
+	coords = PoseStamped()
+
+	if moving:
+		print(moving)
+	else:
+		coords.position.w = 1
+		if (instruction == "GO TO BOOKSELLER"):
+			coords.pose.position.x = 7.0000
+			coords.pose.position.y = -1.0000
+			robopub.publsh(coords)
+		elif(instrucion == "GO TO SAFETY CONE"):
+			coords.pose.position.x = 8.5000
+			coords.pose.position.y = 0.0000
+			robopub.publish(coords)
+		elif(instruction == "GO TO TABLE"):
+			coords.pose.position.x = 8.8000
+			coords.pose.position.y = 8.4500
+			robopub.publish(coords)
+		elif(instruction == "GO TO COUCH"):
+			coords.pose.position.x = 9.0000
+			coords.pose.position.y = 6.4000
+			robopub.publish(coords)
+		else(instruction == "GO TO DESK"):
+			coords.pose.position.x = 3.2000
+			coords.pose.position.y = 6.2000
+			robopub.publish(coords)
+
+def robotalk(talk):
+	Rtalk = SoundRequest()
+	Rtalk.sound = 1
+	Rtalk.volume = 2
+	Rtalk.arg = talk
+
+	robopubtalk.publish(Rtalk)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except rospy.ROSInterruptionexception:
+		pass
