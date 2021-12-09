d08f47c9546e405835ca8009698fb68c54778411
GustavoHerrera97 <panke1997@gmail.com>
2021-12-08 21:30:07 -0600

    final

--- catkin_ws/src/students/scripts/proyectofinal.py
+++ catkin_ws/src/students/scripts/proyectofinal.py
@@ -6,42 +6,43 @@ from sound_play.msg import SoundRequest
 from std_msgs.msg import String
 
 NAME = "Proyecto_Final_Gutierrez_Alcibar_y_Herrera_Cordero"
- 
+moving = False 
 robopub = None
+loop = None
 
 def move_robot(instruction):
 	global moving
 	coords = PoseStamped()
 
-	if moving = True:
+	if moving == True:
 		robotalk(instruction)
+
 	if not moving:
-		coords.position.w = 1
 		if (instruction == "GO TO BOOKSELLER"):
 			coords.pose.position.x = 7.0000
 			coords.pose.position.y = -1.0000
-			robopub.publsh(coords)
+			robopub.publish(coords)
 			robotalk("Moving to " + instruction )
-		elif(instrucion == "GO TO SAFETY CONE"):
+		elif(instruction == "GO TO SAFETY CONE"):
 			coords.pose.position.x = 8.5000
 			coords.pose.position.y = 0.0000
 			robopub.publish(coords)
-			robotalk("Moving to " + intruction)
+			robotalk("Moving to " + instruction)
 		elif(instruction == "GO TO TABLE"):
 			coords.pose.position.x = 8.8000
 			coords.pose.position.y = 8.4500
 			robopub.publish(coords)
-			robotalk("Moving to " + intruction)
+			robotalk("Moving to " + instruction)
 		elif(instruction == "GO TO COUCH"):
 			coords.pose.position.x = 9.0000
 			coords.pose.position.y = 6.4000
 			robopub.publish(coords)
-			robotalk("Moving to " + intruction)
+			robotalk("Moving to " + instruction)
 		elif(instruction == "GO TO DESK"):
 			coords.pose.position.x = 3.2000
 			coords.pose.position.y = 6.2000
 			robopub.publish(coords)
-			robotalk("Moving to " + intruction)
+			robotalk("Moving to " + instruction)
 
 
 def robotalk(talk):
@@ -53,15 +54,22 @@ def robotalk(talk):
 
 	robopubtalk.publish(Rtalk)
 
-def lugar(instruction):
+def lugar(msg):
 	instruction=msg.data
 	move_robot(instruction)
+
+def driver(msg):
+	if(msg.linear.x != 0):
+		moving = False
+	elif (msg.linear.x == 0):
+		moving = True
 
 def main():
 
 	global robopub,robopubtalk,loop
 	rospy.init_node("final")
 	rospy.Subscriber('/recognized',String,lugar)
+	rospy.Subscriber('cmd_vel', Twist,driver)
 	robopubtalk = rospy.Publisher('/robotsound',SoundRequest,queue_size=10) 
 	robopub=rospy.Publisher('/move_base_simple/goal',PoseStamped,queue_size=10)
 	loop = rospy.Rate(20)
