3417475d3892de22bab84ee6c522f2dc0dc49e62
Marvin The Paranoid Android <marvin@hitchhikersguidetothegalaxy.com>
2021-12-09 16:54:53 -0600

    Version 4 proyecto

--- catkin_ws/src/students/scripts/Implementado.py
+++ catkin_ws/src/students/scripts/Implementado.py
@@ -7,45 +7,43 @@ Posicion=None
      
 def asigdestino(Posicion):
     Puntometa= PoseStamped()
-    if(Posicion == "ROBOTINA COME TO KITCHEN"):
+    if(Posicion == "ROBOT COME TO KITCHEN"):
         Puntometa.pose.position.x=4.0
         Puntometa.pose.position.y=5.0
         Puntometa.pose.position.z=0.0
         Puntometa.pose.orientation.w=1.0
         goal_pub.publish(Puntometa)
-    elif(Posicion == "ROBOTINA COME TO BEDROOM"):
+    elif(Posicion == "ROBOT COME TO BEDROOM"):
         Puntometa.pose.position.x=5.0
         Puntometa.pose.position.y=2.0
         Puntometa.pose.position.z=0.0
         Puntometa.pose.orientation.w=1.0
         goal_pub.publish(Puntometa)
-    elif(Posicion == "ROBOTINA COME TO THE BATHROOM"):
+    elif(Posicion == "ROBOT COME TO THE BATHROOM"):
         Puntometa.pose.position.x=0.0
         Puntometa.pose.position.y=0.0
         Puntometa.pose.position.z=0.0
         Puntometa.pose.orientation.w=1.0
         goal_pub.publish(Puntometa)
-    elif(Posicion == "ROBOTINA COME TO STOP"):
+    elif(Posicion == "ROBOT COME TO STOP"):
         Puntometa.pose.position.x=0.0
         Puntometa.pose.position.y=0.0
         Puntometa.pose.position.z=0.0
         Puntometa.pose.orientation.w=1.0
         goal_pub.publish(Puntometa)
 
-def callback_voz(msg):
-    Posicion = msg.data
-    asigpos(Posicion)
 
 
 def main():
     global goal_pub, Posicion
-    
-    rospy.Subscriber("/recognized", String, callback_voz)
+ 
     goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
     rospy.init_node("Implementado")
     rate = rospy.Rate(30)
     rospy.spin()
     
+    rospy.Subscriber("/cmd_vel",Twist,callback_movimiento)
+    goal_pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 
 if __name__ == '__main__':
     try:
