14e64eeab644946a9bbde7d566aa6c7eb6560724
JuarezCastilloA <jucart3.1@gmail.com>
2021-12-09 12:24:28 -0600

    Proyecto Correccion 1

--- catkin_ws/src/students/scripts/practice04_proj.py
+++ catkin_ws/src/students/scripts/practice04_proj.py
@@ -48,7 +48,7 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     if (error_a > math.pi):
         error_a = error_a-2*math.pi
     elif error_a <= -math.pi:
-        error_a = error_a+2*math.p
+        error_a = error_a+2*math.pi
     v = v_max*math.exp(-error_a*error_a/alpha)
     w = w_max*(2/(1 + math.exp(-error_a/beta)) - 1)
     cmd_vel.linear.x=v
